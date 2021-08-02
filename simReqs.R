5a9fa8a5639b559b0cbd8b136333faa47a20bb78
Josue Gomez <josue.gc@oracle.com>
2021-08-02 11:33:10 -0500

    Deleting support file: simReqs.R requerido antes de que migraramos las funciones de corre simulación y aplica formula

--- simReqs.R
+++
@@ -1,6 +0,0 @@
-#load("corre.simulacion.R")
-#load("aplica.formula.R")
-#load("terminos.ecuacion.R")
-# load("datos.completos.R")
-# region <- "SIERRA OCCIDENTAL"
-# load(paste0("proyeccion.agregada",region,".R"))
